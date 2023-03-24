b31aef38da5d03480a6a1b706302ddce16d376a2
Samayp710 <patelsam1120@gmail.com>
2023-03-24 19:48:47 -0400

    commit

--- HW1/Main.java
+++ HW1/Main.java
@@ -3,11 +3,11 @@ package HW1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start of Main");
-        // Create an instance of the Pyramid class and call the Output method (see Pyramid.java for more info)
-
+        // Create an instance of the Pyramid class and call the makePyramid method (see Pyramid.java for more info)
+        Pyramid.Output();
         // Create an instance of the Adlibs class and call the story method (see Adlibs.java for more info)
-
+        Adlibs.story("Samay", 18, "Green"); 
         // Create an instance of the Replace class and call the charReplace method (see Replace.java for more info)
-
+        Replace.charReplace("I MAKE BREAD", 2, 'T');
     }
 }
--- HW1/Pyramid.java
+++ HW1/Pyramid.java
@@ -15,7 +15,8 @@ Hint2: Also notice the method is void meaning you will not be returning anything
 Hint3: The print statement is System.out.println();
 */
 public class Pyramid {
-    public static void Output() {
-        
+    public static void Output() 
+    {
+        System.out.println("#\n##\n###\n####\n#####\n####\n###\n##\n#");
     }
 }
