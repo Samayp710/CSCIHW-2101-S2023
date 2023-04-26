0048c2ea64a9ab435d10a54139b1b71f1da728ce
Samayp710 <patelsam1120@gmail.com>
2023-04-25 22:04:41 -0400

    complete

--- HW5/ArrayListPlayground.java
+++ HW5/ArrayListPlayground.java
@@ -16,6 +16,30 @@ public class ArrayListPlayground {
     // d) set the value of the 2nd element to 6
     // d) Set the value of the 9th element of the array nums to the sum of the 13th and 2nd elements of the array nums.
     
+    ArrayList<Integer> nums = new ArrayList<Integer>(15);
+    nums.add(0);
+    nums.add(1);
+    nums.add(2);
+    nums.add(3);
+    nums.add(4);
+    nums.add(5);
+    nums.add(6);
+    nums.add(7);
+    nums.add(8);
+    nums.add(9);
+    nums.add(10);
+    nums.add(11);
+    nums.add(12);
+    nums.add(13);
+    nums.add(14);
+    System.out.println(nums.get(9));
+    nums.set(4, 99);
+    nums.set(12, 15);
+    nums.set(1, 6);
+    nums.set(8, nums.get(12) + nums.get(1));
+    System.out.println(nums);
+
+
 
 
          
@@ -24,6 +48,18 @@ public class ArrayListPlayground {
     // b) output each of the days of the week
     // c) output the days of the week that we have class 
     // d) change the arrayList to start on Sunday
+
+    ArrayList<String> days = new ArrayList<String>(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"));
+    System.out.println(days);
+    System.out.println(days.get(1) + " " + days.get(3));
+    days.set(0, "Sunday");
+    days.set(1, "Monday");
+    days.set(2, "Tuesday");
+    days.set(3, "Wednesday");
+    days.set(4, "Thursday");
+    days.set(5, "Friday");
+    days.set(6, "Saturday");
+    System.out.println(days);
 
 
     // Question 3 
@@ -52,9 +88,39 @@ public class ArrayListPlayground {
     //             System.out.println();
     //         }
     //     }
-        
+    
+    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    Scanner input = new Scanner(System.in);
+    int num = 1;
+    while(num != 0){
+        System.out.println("Please enter a number to add to the ArrayList or enter 0 to stop adding numbers: ");
+        num = input.nextInt();
+        if(num != 0){
+            numbers.add(num);
    
+        }
+    }
+    System.out.println(numbers);
+    Collections.sort(numbers);
+    System.out.println(numbers);
+    if(numbers.size()%3 != 0){
+        while(numbers.size()%3 != 0){
+            System.out.println("Please enter a number to add to the ArrayList to make it divisable by 3: ");
+            num = input.nextInt();
+            numbers.add(num);
+        }
+    }
+    System.out.println(numbers);
+    for(int i=0;i<numbers.size();i++)
+        {
+            System.out.print(numbers.get(i)+" ");
+            if((i+1)%3==0)
+            {
+                System.out.println();
+            }
 
+        }
+    }
 }
 
 
